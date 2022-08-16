4ea3a755d00680c8ca845f4ffab6ecd63f7833a6
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2022-08-15 20:58:58 -0500

    Add files via upload

---
+++ scripts/sesion01.R
@@ -0,0 +1,65 @@
+saludo <- "Hola a todos los scidateros del curso"
+
+suma <- 5+7
+
+suma*3
+
+suma <- 14
+
+2**4 
+
+
+mi_vector <- c("a","hola","tengo hambre","2")
+
+mi_vector <- c(1,4,56,-2)
+
+mi_vector <- as.character(mi_vector)
+
+mi_vector_dos <- c(mi_vector,c("hola","adiós"))
+mi_vector_dos
+
+
+as.numeric(mi_vector_dos)
+
+
+mi_vector_dos[3]
+
+mi_vector_dos[5]
+
+mi_vector_dos[c(1,4,5)]
+
+mi_vector_dos[c(3,6)]
+
+numeros <- 1:100
+
+numeros
+
+mi_vector_dos[c(3,4,5)]
+mi_vector_dos[c(3:5)]
+
+mi_vector_numerico <- mi_vector_dos[1:4]
+mi_vector_numerico <- as.numeric(mi_vector_numerico)
+
+mis_logicos <- (mi_vector_numerico > 2.5)
+
+mi_vector_numerico[mis_logicos]
+
+mi_vector_numerico[mi_vector_numerico>2] <- 0
+
+mi_vector_numerico
+
+names(mi_vector_numerico) <- c("Luis","Diego","Yasser","MD")
+
+mi_vector_numerico
+
+mi_vector_numerico[c("Luis","MD")]
+
+
+length(mi_vector)
+
+length(otro_vector)
+
+un_vector_raro <- c("hola",FALSE,2022,13,15,92929,FALSE)
+
+
+
