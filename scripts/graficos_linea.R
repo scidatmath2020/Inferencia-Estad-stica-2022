c00587a72a5696ec6bab16a63f8d9412b708fc2a
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2022-08-22 16:35:29 -0500

    Add files via upload

--- scripts/graficos_linea.R
+++ scripts/graficos_linea.R
@@ -1,5 +1,6 @@
 setwd("C:/Users/hp master/OneDrive/Escritorio/mi_data")
 library(ggplot2)
+library(tidyverse)
 
 fecha_sintomas <- read.csv("fecha_sintomas.csv")
 
@@ -20,6 +21,22 @@ tabla_delitos$Fecha <- as.yearmon(tabla_delitos$Fecha,"%b %Y")
 ggplot(data = tabla_delitos) +
   geom_line(mapping = aes(Fecha,total,color=tipo))
 
+
+tabla_clase_ancha <- read.csv("tabla_clase_ancha.csv")
+tabla_clase_ancha$fecha <- as.yearmon(tabla_clase_ancha$fecha,"%b %Y")
+
+ggplot(data = tabla_clase_ancha) +
+  geom_line(mapping = aes(x=fecha,y=extorsion_amenazas),color = "red") +
+  geom_line(mapping = aes(x=fecha,y=sexuales),color = "blue") 
+
+tabla_clase_larga <- read.csv("tabla_clase_larga.csv")
+tabla_clase_larga$fecha <- as.yearmon(tabla_clase_larga$fecha,"%b %Y")
+
+ggplot(data = tabla_clase_larga) +
+  geom_line(mapping = aes(x=fecha,y=total,color = clase)) 
+
+#####################################################
+#####################################################
 #####################################################
 
 delitos_mes <- read.csv("delitos_mensuales.csv")
@@ -27,6 +44,9 @@ delitos_mes$mes <- as.yearmon(delitos_mes$mes, "%b %Y")
 delitos_mes$anio <- as.factor(delitos_mes$anio)
 delitos_mes$month <- substr(delitos_mes$mes,1,4)
 delitos_mes$month <- as.yearmon(delitos_mes$month, "%b")
+
+ggplot(data = delitos_mes,mapping = aes(x = mes, y = total)) +
+  geom_line(color = "blue")
 
 ggplot(data = delitos_mes,mapping = aes(x = mes, y = total,color = anio)) +
   geom_line()
@@ -39,6 +59,8 @@ ggplot(data = delitos_mes,mapping = aes(x = month, y = total,color=anio)) +
   facet_grid(anio~.)
   
 
+##########################################
+##########################################
 ##########################################
 
 robo_calle <- data.frame(mes = delitos_mes$mes,
